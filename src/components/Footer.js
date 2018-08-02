69a530542deb34d2e136f62ccb4d353f0113dcd9
saladsnake <mifrent@hotmail.com>
2018-08-02 21:18:18 +1000

    i changed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 
 const Footer = () => (
-  <footer>
-    Created by <a href="https://www.linkedin.com/in/jeffersonribeiro123/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">Jefferson Ribeiro</a>
-  </footer>
+    <footer>
+        Created by <a href="https://www.rmit.edu.au/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">Richard
+        Stallman's School of Dance</a>
+    </footer>
 );
 
 export default Footer;
