6c1b448bef424f6a3a176fc3b47fb60a32e94517
will <s3660470@student.rmit.edu.au>
2018-08-17 15:34:06 +1000

    fixed filter array and added filter-by-color

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -52,16 +52,20 @@ export const fetchProducts = (filters, sortBy, callback) => dispatch => {
               let filt = filters[i];
               if (filt.substring(0,2) == 'S_') {
                   sizeFilters.push(filt.substring(2));
+              } else if (filt.substring(0,2) == 'C_') {
+                  colorFilters.push(filt.substring(2));
+              } else if (filt.substring(0,2) == 'G_') {
+                  genderFilters.push(filt.substring(2));
               }
           }
       }
 
 
-      console.log("SIZES");
-      console.log(sizeFilters);
-
-      console.log("PRODUCTS");
-      console.log(products);
+      // console.log("SIZES");
+      // console.log(sizeFilters);
+      //
+      // console.log("PRODUCTS");
+      // console.log(products);
 
       //Re-check array length and apply size filters
       if(!!sizeFilters && sizeFilters.length > 0) {
@@ -70,15 +74,26 @@ export const fetchProducts = (filters, sortBy, callback) => dispatch => {
       }
 
       //Apply gender filters (in glorious ES5)
-      // if (!!genderFilters && genderFilters.length > 0) {
-      //     products = products.filter(function (p) {
-      //         return genderFilters.find(function (f) {
-      //             return p.availableGenders.find(function(gender) {
-      //                 return (gender == f);
-      //             })
-      //         })
-      //     });
-      // }
+      if (!!genderFilters && genderFilters.length > 0) {
+          products = products.filter(function (p) {
+              return genderFilters.find(function (f) {
+                  return p.availableGenders.find(function(gender) {
+                      return (gender == f);
+                  })
+              })
+          });
+      }
+
+      //Apply color filters
+      if (!!colorFilters && colorFilters.length > 0) {
+          products = products.filter(function (p) {
+              return colorFilters.find(function (f) {
+                  return p.availableColors.find(function(color) {
+                      return (color == f);
+                  })
+              })
+          });
+      }
 
 
       if(!!sortBy){
