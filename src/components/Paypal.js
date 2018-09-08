a6204169059e05ccc551c3316169a6e60baea062
saladsnake <mifrent@hotmail.com>
2018-09-09 01:07:51 +1000

    done

--- src/components/Paypal.js
+++ src/components/Paypal.js
@@ -10,7 +10,17 @@ class Paypal extends React.Component {
 
         const onSuccess = (payment) => {
             // Congratulation, it came here means everything's fine!
-            console.log("The payment was succeeded!", payment);
+            console.log("The payment is all good!", payment);
+
+            if (payment.paid = true) {
+                let hackString = "Thankyou for your order, we will deliver it to:\n"
+                    + payment.address.line1 + ", " + payment.address.postal_code + ", " + payment.address.city
+                    + ", " + payment.address.state
+                    + "\n\nYou have paid a total of $" + total
+                    + "\n\nThe receipt will be emailed to " + payment.email;
+                alert(hackString)
+
+            }
         };
 
         // mifrent-buyer@hotmail.com is the buyer email
@@ -22,7 +32,7 @@ class Paypal extends React.Component {
 
         return (
             <div>
-                <PaypalExpressBtn env={env} client={client} currency={currency} total={total}
+                <PaypalExpressBtn onSuccess={onSuccess} env={env} client={client} currency={currency} total={total}
                                   style={{layout: 'vertical', size: 'medium', color: 'gold', shape: 'rect'}}
                                   funding={{allowed: 'paypal.FUNDING.CARD, paypal.FUNDING.CREDIT'}}
                 />
