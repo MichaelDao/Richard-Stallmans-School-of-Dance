d10eb40dc5eb7a38eb3d37d03a70e7ec3aecf551
William Cohen <s3660470@student.rmit.edu.au>
2018-10-12 15:47:09 +1100

    added api tests

---
+++ test.js
@@ -0,0 +1,5 @@
+try {
+    console.log(JSON.parse('{\"id\": stringid}'));
+} catch {
+    console.log('INVALID JSON');
+}
---
+++ unit-tests/ApiCalls.js
@@ -0,0 +1,67 @@
+// Mocha Unit Tester
+const assert = require('assert')
+const fetch = require('node-fetch');
+
+const rating = {productId: 32, productRating: 5};
+const sale = [{id: 32, quantity: 1}];
+
+//Same as '../src/store/actions/apiActions'
+function callApi(action, data, callback) {
+    //https://api-dot-rmit-shoppingcart.appspot.com/
+    fetch('http://127.0.0.1:8001/' + action, {
+        method: 'POST',
+        body: JSON.stringify(data, null, 2), // Prettify the JSON output
+        headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        callback(data);
+    }).catch(err => {
+        //console.error(err);
+    });
+}
+
+function isValidJson(json) {
+    try {
+        JSON.parse(json);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+describe('Api Calls', function()
+{
+    it('Should return a sample test response JSON on \'test\' call', function()
+    {
+      callApi('test', {message: 'hello'}, function (res) {
+          assert.equal(res.test, 'Test response');
+          done();
+      });
+    });
+
+    it('Should return a reccomendation on \'reccomend\' call', function()
+    {
+      callApi('reccomend', {}, function (res) {
+          assert.equal(res.id, true);
+          done();
+      });
+    });
+
+    it('Should return a success on \'sale\' call', function()
+    {
+      callApi('sale', sale, function (res) {
+          assert.equal(res, true);
+          done();
+      });
+    });
+
+    it('Should return a success on \'rating\' call', function()
+    {
+      callApi('rating', rating, function (res) {
+          assert.equal(res.rating, true);
+          done();
+      });
+    });
+
+});
